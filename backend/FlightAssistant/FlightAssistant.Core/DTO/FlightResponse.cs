918bec8c8b211891d67572f0234a7639d9ddfc3a
nkaran <nixa1995kt@gmail.com>
2023-03-12 19:14:58 +0100

    dto fixes

--- backend/FlightAssistant/FlightAssistant.Core/DTO/FlightResponse.cs
+++ backend/FlightAssistant/FlightAssistant.Core/DTO/FlightResponse.cs
@@ -6,21 +6,21 @@
         public int DepartureAirportId { get; set; }
         public int ArrivalAirportId { get; set; }
         public DateTime DepartureDate { get; set; }
-        public DateTime? ArrivalDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
         public DateTime? ReturnDate { get; set; }
         public int NumberOfPassangers { get; set; }
         public int NumberOfLayovers { get; set; }
-        public int? CurrencyId { get; set; }
+        public int CurrencyId { get; set; }
         public string GrandTotalPrice { get; set; }
 
-        public FlightResponse(int id, int departureAirportId, int arrivalAirportId, DateTime departureDate, DateTime? arrivalDate, DateTime? returnDate, int numberOfPassangers, int numberOfLayovers, int? currencyId, string grandTotalPrice)
+        public FlightResponse(int id, int departureAirportId, int arrivalAirportId, DateTime departureDate, DateTime arrivalDate, DateTime returnDate, int numberOfPassangers, int numberOfLayovers, int currencyId, string grandTotalPrice)
         {
             Id = id;
             DepartureAirportId = departureAirportId;
             ArrivalAirportId = arrivalAirportId;
             DepartureDate = departureDate;
             ArrivalDate = arrivalDate;
-            ReturnDate = returnDate;
+            ReturnDate = returnDate.Date != DateTime.MinValue.Date ? returnDate.Date : null;
             NumberOfPassangers = numberOfPassangers;
             NumberOfLayovers = numberOfLayovers;
             CurrencyId = currencyId;
